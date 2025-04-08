9a8ebfd7f6ace5b8e5f66779f1f1b6a0147dab98
Osio Aliu <oaliu@hotmail.com>
2025-04-09 01:50:05 +0200

    WIP

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'Osio_MultipleCategoryImages/js/form/element/image-uploader': 'Osio_MultipleCategoryImages/js/form/element/image-uploader'
+        }
+    }
+};
