191117be583ffda0851d79b4495cd5efc6f177e3
Osio Aliu <oaliu@hotmail.com>
2025-04-09 09:51:33 +0200

    refresh

--- view/adminhtml/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Osio_MultipleCategoryImages/js/form/element/image-uploader': 'Osio_MultipleCategoryImages/js/form/element/image-uploader'
-        }
-    }
-};
